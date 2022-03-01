c23238bd395a1750595c69d4d121e3687bec3496
shin-ch13 <soccer.nobu@gmail.com>
2022-03-01 22:37:11 +0900

    add dockercmd_module.py

---
+++ Test-Infrastracture/Docker/docker-and-ipcmd_example/dockercmd_module.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+import sys
+import os
+import subprocess
+import json
+
+class DockerCommand:
+  def check_compose_file(self):
+    path = 'docker-compose.yml'
+    is_file = os.path.isfile(path)
+    if not is_file:
+      print(f"{path} not found in current directory")
+      sys.exit(1)
+
+  def get_container_service(self,services):
+    container_services = []
+    if 'ALL' in services:
+      proc = subprocess.run(['docker-compose','ps','--format=json'],stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+      json_dict = json.loads(proc.stdout.decode('utf8'))
+      if not json_dict:
+        print('docker container not up')
+        sys.exit(1)
+      else:
+        for i in range(len(json_dict)):
+          container_service = json_dict[i]['Service']
+          container_services.append(container_service)
+    else:
+      for service in services:
+        proc = subprocess.run(['docker-compose','ps',service,'--format=json'],stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+        if proc.returncode == 0:
+          json_dict = json.loads(proc.stdout.decode('utf8'))
+          container_service = json_dict[0]['Service']
+          container_services.append(container_service)
+        else:
+          print('{}'.format(proc.stderr.decode('utf8')))
+          sys.exit(1)
+    return container_services
+
+  def get_container_name(self,container_service):
+    proc = subprocess.run(['docker-compose','ps',container_service,'--format=json'],stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    if proc.returncode == 0:
+      json_dict = json.loads(proc.stdout.decode('utf8'))
+      container_name = json_dict[0]['Name']
+    else:
+      print('{}'.format(proc.stderr.decode('utf8')))
+      sys.exit(1)
+    return container_name
+
+  def get_container_id(self,container_name):
+    proc = subprocess.run(['docker','inspect',container_name,'--format','{{.Id}}'],stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    if proc.returncode == 0:
+      container_id= proc.stdout.decode('utf8').strip()
+    else:
+      print('{}'.format(proc.stderr.decode('utf8')))
+      sys.exit(1)
+    return container_id
+
+  def get_container_pid(self,container_id):
+    proc = subprocess.run(['docker','inspect',container_id,'--format','{{.State.Pid}}'],stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    if proc.returncode == 0:
+      container_pid = proc.stdout.decode('utf8').strip()
+      if os.path.isfile('/proc/{}/ns/net'.format(container_pid)):
+        container_pid_path = '/proc/{}/ns/net'.format(container_pid)
+      else:
+        print('/proc/{}/ns/net file not found'.format(container_pid))
+        sys.exit(1)
+    else:
+      print('{}'.format(proc.stderr.decode('utf8')))
+      sys.exit(1)
+    return container_pid,container_pid_path
